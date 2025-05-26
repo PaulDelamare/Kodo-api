55223df12ffdf780199032653fca52942e7c75bd
Paul_Delamare <paulodela2006@gmail.com>
2025-05-26 17:50:00 +0200

    ✨ Suppression de l'importation inutile de hello.routes dans auth.routes

--- src/Routes/auth.routes.ts
+++ src/Routes/auth.routes.ts
@@ -1,6 +1,6 @@
+import { router } from "../../config/router.config";
 import { AuthController } from "../Controllers/auth.controller";
 import { checkApiKey } from "../Utils/checkApiKey/checkApiKey";
-import router from "./hello.routes";
 
 router.post('/register', checkApiKey(), AuthController.register);
 
