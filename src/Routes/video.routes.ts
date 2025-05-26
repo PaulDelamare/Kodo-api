1ccd9bd1cedc8a31b38197ad1896ab0282ed70a7
PaulDelamare <paulodela2006@gmail.com>
2025-05-26 19:26:16 +0200

    Correction de la syntaxe dans le fichier des routes vidéo

--- src/Routes/video.routes.ts
+++ src/Routes/video.routes.ts
@@ -1,13 +1,10 @@
-// ! IMPORT
 import { router } from "../../config/router.config";
 import { VideoController } from "../Controllers/video.controller";
 import { checkApiKey } from "../Utils/checkApiKey/checkApiKey";
 import { checkAuth } from "../Utils/validatedLogin/validatedLogin";
 const multer = require("multer");
-const upload = multer().single("video")
-     ;
+const upload = multer().single("video");
 
 router.post('/video', checkApiKey(), checkAuth(), upload, VideoController.create);
 
-
-export default router
+export default router;
