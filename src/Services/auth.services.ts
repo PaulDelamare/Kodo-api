0172492928ca6e7cd5ba350af1db932c8cbe7735
Paul_Delamare <paulodela2006@gmail.com>
2025-05-28 14:33:46 +0200

    Ajouter des services pour la génération et la vérification des tokens de réinitialisation de mot de passe

--- src/Services/auth.services.ts
+++ src/Services/auth.services.ts
@@ -1,8 +1,10 @@
-import { User } from "@prisma/client";
+import { PasswordResetToken, User } from "@prisma/client";
 import { hashPassword, verifyPassword } from "../Utils/hashPassword/hashPassword";
 import { bdd } from "../../config/prismaClient.config";
 import { UserServices } from "./user.services";
 import jwt, { JwtPayload } from "jsonwebtoken"
+import { throwError } from "../Utils/errorHandler/errorHandler";
+import { generateToken } from "../Utils/genarateToken/genarateToken";
 
 /**
  * Crée un nouvel utilisateur dans la base de données.
@@ -60,7 +62,60 @@ const generateTokens = (payload: JwtPayload, secret: string, expiresIn: "30d" | 
     )
 }
 
+/**
+ * Génére un nouveau token de réinitialisation de mot de passe.
+ *
+ * La fonction prend en param tre un objet contenant l'email d'un utilisateur.
+ * Elle recherche l'utilisateur correspondant dans la base de donn es.
+ * Si l'utilisateur n'existe pas, elle renvoie une erreur 200.
+ * Sinon, elle génére un nouveau token de réinitialisation de mot de passe,
+ * le stocke dans la base de donn es, et le renvoie.
+ * @param validatedData - L'email de l'utilisateur.
+ * @returns - Les informations du token de réinitialisation de mot de passe.
+ */
+const generatePasswordResetService = async (validatedData: Pick<User, 'email'>): Promise<{ token: string; user: User }> => {
+    const user = await bdd.user.findUnique({ where: validatedData });
+    if (!user) return throwError(200, 'Un email a été envoyé');
+
+    const token = generateToken(32);
+    const expiresAt = new Date(Date.now() + 60 * 60 * 1000);
+
+    await bdd.$transaction([
+        bdd.passwordResetToken.deleteMany({ where: { userId: user.id } }),
+        bdd.passwordResetToken.create({ data: { userId: user.id, token, expiresAt } })
+    ]);
+
+    return { token, user };
+};
+
+const checkUserRequest = async (validatedData: Pick<PasswordResetToken, 'token' | 'userId'>): Promise<void> => {
+
+    const passwordResetToken = await bdd.passwordResetToken.findUnique({
+        where: validatedData
+    });
+
+    if (!passwordResetToken) {
+        return throwError(404, 'Aucune demande de reinitialisation de mot de passe trouvée');
+    }
+}
+
+const changePassword = async (validatedData: Pick<PasswordResetToken, 'token' | 'userId'> & Pick<User, 'password'>): Promise<void> => {
+    const { token, userId, password } = validatedData;
+
+    await checkUserRequest({ token, userId });
+
+    await bdd.user.update({
+        where: { id: userId },
+        data: { password: await hashPassword(password) }
+    });
+
+    await bdd.passwordResetToken.delete({ where: { token } });
+}
+
 export const AuthServices = {
     registerService,
-    loginService
+    loginService,
+    generatePasswordResetService,
+    checkUserRequest,
+    changePassword
 }
